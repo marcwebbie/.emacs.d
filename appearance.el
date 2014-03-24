93984d1ecdbd489717a3da563415ea71cb7cf554
marcwebbie <marcwebbie@gmail.com>
2014-03-24 10:14:59 -0300

    Extracted appearance code from init

---
+++ appearance.el
@@ -0,0 +1,20 @@
+(setq visible-bell t
+      font-lock-maximum-decoration t
+      color-theme-is-global t
+      truncate-partial-width-windows nil)
+
+;; Highlight current line
+(global-hl-line-mode 1)
+
+;; Highlight matching parentheses when the point is on them.
+(show-paren-mode 1)
+
+(when window-system
+  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
+  (tooltip-mode -1)
+  (blink-cursor-mode -1))
+
+(load-theme 'cyberpunk :no-confirm)
+
+(use-package powerline
+  :init (powerline-default-theme))
--- init.el
+++ init.el
@@ -24,33 +24,9 @@
 
 (load-local "sane-defaults")
 (load-local "defuns")
+(load-local "appearance")
 (load-local "hippie")
 (load-local "keybindings")
-
-
-;; ===========================================
-;; Setup appearance
-;; ===========================================
-(setq visible-bell t
-      font-lock-maximum-decoration t
-      color-theme-is-global t
-      truncate-partial-width-windows nil)
-
-;; Highlight current line
-(global-hl-line-mode 1)
-
-;; Highlight matching parentheses when the point is on them.
-(show-paren-mode 1)
-
-(when window-system
-  (setq frame-title-format '(buffer-file-name "%f" ("%b")))
-  (tooltip-mode -1)
-  (blink-cursor-mode -1))
-
-(load-theme 'solarized-dark :no-confirm)
-
-(use-package powerline
-  :init (powerline-default-theme))
 
 
 ;; ===========================================
