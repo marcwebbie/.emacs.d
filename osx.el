e4bccd848f33bd60b14ce5b43d1d490b40d0ca29
marcwebbie <marcwebbie@gmail.com>
2014-05-31 05:00:02 -0300

    Fixed ignoring DS store for ido

--- osx.el
+++ osx.el
@@ -19,9 +19,6 @@
 (setq delete-by-moving-to-trash t
       trash-directory "~/.Trash/emacs")
 
-;; Ignore .DS_Store files with ido mode
-(add-to-list 'ido-ignore-files "\\.DS_Store")
-
 (defun finder ()
   "Opens file directory in Finder."
   (interactive)
