a3011a422458d002c08ec3c01cf447d5b5560663
marcwebbie <marcwebbie@gmail.com>
2014-05-31 04:21:11 -0300

    Rewriting misc config

--- misc.el
+++ misc.el
@@ -1,87 +1,114 @@
-;;; misc.el --- Misc configuration -*- lexical-binding: t; -*-
-
-;; Do not pause on redisplay
-(setq redisplay-dont-pause t)
+;; Allow pasting selection outside of Emacs
+(setq x-select-enable-clipboard t)
 
 ;; Do not make any backup files
 (setq make-backup-files nil)
-
-;; Kill whole line
-(setq kill-whole-line t)
-
-;; Do not show startup message
-(setq inhibit-startup-message t)
-
-;; Show keystrokes in minibuffer early
-(setq echo-keystrokes 0.1)
 
 ;; Set default browser
 (setq browse-url-browser-function 'browse-url-generic)
 (setq browse-url-generic-program "firefox")
 
-;; Initial major mode is Emacs Lisp mode
-(setq initial-major-mode 'emacs-lisp-mode)
+;; Set default browser
+(setq browse-url-browser-function 'browse-url-generic)
+(setq browse-url-generic-program "firefox")
 
-;; Indent with spaces instead of tabs
-(setq-default indent-tabs-mode nil)
+;; Highlight symbol at point
+;(add-hook 'find-file-hook 'idle-highlight-mode)
 
-;; Truncate lines
-(set-default 'truncate-lines t)
+;; Auto refresh buffers
+(global-auto-revert-mode 1)
 
-;; Do not blink cursor
-(blink-cursor-mode -1)
+;; Do not show startup message
+(setq inhibit-startup-message t)
 
-;; Do not show any tooltips
-(tooltip-mode -1)
+;; Also auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
 
-;; Remove selected region if typing
-(pending-delete-mode 1)
+;; Show keystrokes in progress
+(setq echo-keystrokes 0.1)
+
+;; Move files to trash when deleting
+(setq delete-by-moving-to-trash t)
+
+;; Real emacs knights don't use shift to mark things
+(setq shift-select-mode nil)
+
+;; Transparently open compressed files
+(auto-compression-mode t)
+
+;; Enable syntax highlighting for older Emacsen that have it off
+(global-font-lock-mode t)
+
+;; Answering just 'y' or 'n' will do
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; UTF-8 please
+(setq locale-coding-system 'utf-8) ; pretty
+(set-terminal-coding-system 'utf-8) ; pretty
+(set-keyboard-coding-system 'utf-8) ; pretty
+(set-selection-coding-system 'utf-8) ; please
+(prefer-coding-system 'utf-8) ; with sugar on top
+
+;; Show active region
+(transient-mark-mode 1)
+(make-variable-buffer-local 'transient-mark-mode)
+(put 'transient-mark-mode 'permanent-local t)
+(setq-default transient-mark-mode t)
 
 ;; Remove text in active region if inserting text
 (delete-selection-mode 1)
 
-;; Allow some commands
-(dolist (command '(narrow-to-region downcase-region upcase-region))
-  (put command 'disabled nil))
+;; Don't highlight matches with jump-char - it's distracting
+(setq jump-char-lazy-highlight-face nil)
 
-;; Prefer utf8
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+;; Always display line and column numbers
+(setq line-number-mode t)
+(setq column-number-mode t)
 
-;; Do not ask for confirmation
-(setq confirm-nonexistent-file-or-buffer nil)
+;; Lines should be 80 characters wide, not 72
+(setq fill-column 80)
 
-(setq kill-buffer-query-functions
-      (remq 'process-kill-buffer-query-function
-            kill-buffer-query-functions))
+;; Save a list of recent files visited. (open recent file with C-x f)
+(recentf-mode 1)
+(setq recentf-max-saved-items 100) ;; just 20 is too recent
 
-;; Highlight symbol at point
-(add-hook 'find-file-hook 'idle-highlight-mode)
-
-(defalias 'dtw 'delete-trailing-whitespace)
-(defalias 'yes-or-no-p 'y-or-n-p)
-
-;; Auto refresh buffers
-(global-auto-revert-mode t)
-
-;; Auto refresh dired, but be quiet about it
-(setq global-auto-revert-non-file-buffers t)
-(setq auto-revert-verbose nil)
-
-;; Show trailing whitespace
-(setq-default show-trailing-whitespace 0)
-
-;; Do not show annoying menu-bar tips
-(setq suggest-key-bindings nil)
-
-;; Make default mode Emacs lisp mode
-(setq default-major-mode 'emacs-lisp-mode)
-
-;; Show column number in mode line
-(column-number-mode 1)
+;; Save minibuffer history
+(savehist-mode 1)
+(setq history-length 1000)
 
 ;; Undo/redo window configuration with C-c <left>/<right>
 (winner-mode 1)
+
+;; Never insert tabs
+(set-default 'indent-tabs-mode nil)
+
+;; Show me empty lines after buffer end
+(set-default 'indicate-empty-lines t)
+
+;; Easily navigate sillycased words
+(global-subword-mode 1)
+
+;; Don't break lines for me, please
+(setq-default truncate-lines t)
+
+;; Allow recursive minibuffers
+(setq enable-recursive-minibuffers t)
+
+;; Don't be so stingy on the memory, we have lots now. It's the distant future.
+(setq gc-cons-threshold 20000000)
+
+;; org-mode: Don't ruin S-arrow to switch windows please (use M-+ and M-- instead to toggle)
+(setq org-replace-disputed-keys t)
+
+;; Fontify org-mode code blocks
+(setq org-src-fontify-natively t)
+
+;; Sentences do not need double spaces to end. Period.
+(set-default 'sentence-end-double-space nil)
+
+;; 80 chars is a good width.
+(set-default 'fill-column 80)
+
+;; Show trailing whitespace
+(setq-default show-trailing-whitespace 0)
