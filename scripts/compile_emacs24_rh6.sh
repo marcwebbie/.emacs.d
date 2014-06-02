9399d5c99d7f3b0d02cb6af4b5aa32841f28fe45
marcwebbie <marcwebbie@gmail.com>
2014-06-01 21:23:14 -0300

    Adding shebang line to rh6 compile script

--- scripts/compile_emacs24_rh6.sh
+++ scripts/compile_emacs24_rh6.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+
 # install needed libraries
 sudo yum install texinfo libXpm-devel giflib-devel libtiff-devel libotf-devel libjpeg-devel
 
 # compile autoconf
+echo "Download and compiling autoconf..."
 cd /tmp
 curl -L http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz | tar zx
 cd autoconf-2.69/
@@ -10,6 +13,7 @@ cd ..
 rm -rf autoconf-2.69
 
 # compile emacs 24.3
+echo "Download and compiling emacs..."
 cd /tmp
 curl -L http://mirrors.syringanetworks.net/gnu/emacs/emacs-24.3.tar.gz
 cd emacs-24.3/
