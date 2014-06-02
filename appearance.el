49ea92b07184c44db01724267632937c8ef78cf9
marcwebbie <marcwebbie@gmail.com>
2014-06-02 00:57:58 -0300

    Added font choosing logic for osx and linux

--- appearance.el
+++ appearance.el
@@ -13,11 +13,27 @@
       color-theme-is-global t
       truncate-partial-width-windows nil)
 
+(defun set-best-font ()
+  (when (string-equal system-type "darwin")
+    (set-frame-font (cond ((member "Monaco" (font-family-list)) "Monaco-14")
+                          ((member "Ubuntu Mono" (font-family-list)) "Ubuntu Mono-14")
+                          ((member "Inconsolata" (font-family-list)) "Inconsolata-14")
+                          ((member "Menlo" (font-family-list)) "Menlo-14"))) t t)
+  (when (string-equal system-type "gnu/linux")
+    (set-frame-font (cond ((member "Ubuntu Mono" (font-family-list)) "Ubuntu Mono-12")
+                          ((member "Inconsolata" (font-family-list)) "Inconsolata-12")
+                          ((member "Monaco" (font-family-list)) "Monaco-12")
+                          ((member "DejaVu Sans Mono" (font-family-list)) "DejaVu Sans Mono-12")
+                          ((member "Monospace" (font-family-list)) "Monospace-12"))) t t))
+
 (when window-system
   (setq frame-title-format '(buffer-file-name "%f" ("%b")))
   (tooltip-mode -1)
-  (blink-cursor-mode -1))
+  (blink-cursor-mode -1)
+  ;; Choose best font when in windows system
+  (set-best-font))
 
-(add-hook 'emacs-lisp-mode-hook
-          (lambda()
-            (setq mode-name "el")))
+;; Shorten Major mode names
+(add-hook 'emacs-lisp-mode-hook (lambda() (setq mode-name "el")))
+
+;;;
--- mac.el
+++ mac.el
@@ -7,10 +7,9 @@
 (setq mac-command-modifier 'meta)
 (setq ns-function-modifier 'hyper)
 
-;; mac friendly font
-(when window-system
-  (setq default-font "-apple-Monaco-medium-normal-normal-*-15-*-*-*-m-0-iso10646-1")
-  (set-face-attribute 'default nil :font default-font))
+;; OSX friendly font
+;; (when window-system
+;;   (set-face-attribute 'default nil :font "-apple-Monaco-medium-normal-normal-*-14-*-*-*-m-0-iso10646-1"))
 
 ;; Make the browser the OS X default
 (setq browse-url-browser-function 'browse-url-default-macosx-browser)
