29cf6aec1defb3692cad72b1e83e963d49863378
marcwebbie <marcwebbie@gmail.com>
2014-06-10 17:06:41 -0300

    Fixed jstestdriver printing error test not found

--- jstestdriver.el
+++ jstestdriver.el
@@ -10,7 +10,9 @@
   (save-excursion
     (let* ((test_name (when (search-backward-regexp "[ \t]+test\(['\"]\\(.*?\\)['\"]," nil t) (match-string-no-properties 1)))
            (test_cmd (concat "bundle exec rake \"test:jstestdriver:ove_jquery[" test_name "]\"")))
-      (if test_name (compilation-start test_cmd)) (error "Test name not found!"))))
+      (if test_name
+          (compilation-start test_cmd)
+        (error "Test name not found!")))))
 
 (provide 'jstestdriver)
 ;;; jstestdriver.el ends here
