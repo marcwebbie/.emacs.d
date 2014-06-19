f7ed140b0cc535a4acbfe8fd407cc18739333931
marcwebbie <marcwebbie@gmail.com>
2014-06-19 01:05:52 -0300

    Added custom faces for smartparens

--- init.el
+++ init.el
@@ -298,18 +298,14 @@
 
 
 ;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
 ;;  )
-;; (custom-set-faces
-;;  ;; custom-set-faces was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  ;; '(font-lock-function-name-face ((t (:bold t))))
-;;  '(font-lock-keyword-face ((t (:bold t)))))
+(custom-set-faces
+ ;; '(font-lock-function-name-face ((t (:bold t))))
+ ;; '(font-lock-keyword-face ((t (:bold t)))))
+  '(web-mode-html-tag-face ((t (:foreground "#ffb070" :background nil)))))
+  '(sp-show-pair-match-face ((t (:foreground magenta :bold t))))
+  '(sp-show-pair-mismatch-face ((t (:foreground "#ff0000"))))
+ )
 
 
 ;;;; ends init.el
