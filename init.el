d25c59a84a31ef18a6bc736f7eb20158e8251fb5
marcwebbie <marcwebbie@gmail.com>
2014-06-18 23:02:36 -0300

    Added commented custom variables

--- init.el
+++ init.el
@@ -294,4 +294,20 @@
 ;;;; Bindings
 (load-local "key-bindings")
 
+
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  )
+;; (custom-set-faces
+;;  ;; custom-set-faces was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  ;; '(font-lock-function-name-face ((t (:bold t))))
+;;  '(font-lock-keyword-face ((t (:bold t)))))
+
+
 ;;;; ends init.el
