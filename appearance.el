8db446a38e7c6553073c5d3e90410aab209e65e5
marcwebbie <marcwebbie@gmail.com>
2014-12-03 00:25:26 -0300

    Updating font order for osx

--- appearance.el
+++ appearance.el
@@ -16,7 +16,9 @@
 (defun set-best-font ()
   (when (string-equal system-type "darwin")
     (set-frame-font (cond
+                     ((member "Input Mono Narrow" (font-family-list)) "Input Mono Narrow-12")
                      ((member "Inconsolata" (font-family-list)) "Inconsolata-14")
+                     ((member "Source Code Pro" (font-family-list)) "Source Code Pro-12")
                      ((member "Menlo" (font-family-list)) "Menlo-12")
                      ((member "Ubuntu Mono" (font-family-list)) "Ubuntu Mono-14")
                      ((member "Monaco" (font-family-list)) "Monaco-14")
