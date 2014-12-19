301c04ed83cff942a00aaca7528ecd52668cadeb
marcwebbie <marcwebbie@gmail.com>
2014-12-19 03:33:11 -0300

    Support python virtualenv

--- init.el
+++ init.el
@@ -192,10 +192,15 @@
 
 (use-package puppet-mode)
 
+(use-package pyenv-mode)
+
 (use-package python
   :config
   (progn
-    ;; (global-pyenv-mode)
+    (add-hook 'python-mode-hook
+              '(lambda () (pyenv-mode)))
+    (add-hook 'python-mode-hook
+              '(lambda () (anaconda-mode)))
     (add-hook 'python-mode-hook
           '(lambda ()
              (flycheck-mode)
@@ -286,6 +291,13 @@
 (use-package subword
   :diminish subword-mode)
 
+(use-package virtualenvwrapper
+  :init
+  (progn
+    (venv-initialize-interactive-shells) ;; interactive shell support
+    (venv-initialize-eshell) ;; eshell support
+    (setq venv-location "~/.virtualenvs/")))
+
 (use-package visual-regexp
   :config
   (progn
@@ -328,8 +340,23 @@
 
 
 ;;;; Custom variables
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load-local "custom")
+;; (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;; (load-local "custom")
 
 (provide 'init)
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(magit-use-overlays nil)
+ '(paradox-github-token t)
+ '(python-shell-interpreter "ipython")
+ '(python-shell-interpreter-args ""))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
