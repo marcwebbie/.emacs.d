ccaca17c6aa1dccc859cfa38741ae0b731822246
marcwebbie <marcwebbie@gmail.com>
2014-12-25 19:34:22 -0300

    Clean defaults

--- defaults.el
+++ defaults.el
@@ -1,9 +1,5 @@
 ;; Redraw the display before it processes queued input events
-;;(setq redisplay-dont-pause t)
-
-;; Do not make any backup files
-(setq make-backup-files nil) ; stop creating those backup~ files
-(setq auto-save-default nil) ; stop creating those #autosave# files
+(setq redisplay-dont-pause t)
 
 ;; Do not show annying menu-bar tips
 (setq suggest-key-bindings nil)
@@ -25,18 +21,6 @@
 (setq browse-url-browser-function 'browse-url-generic)
 (setq browse-url-generic-program "firefox")
 
-;; Highlight current line
-(global-hl-line-mode 1)
-
-;; Do not show any tooltips
-(tooltip-mode -1)
-
-;; Auto refresh buffers
-(global-auto-revert-mode 1)
-
-;; Do not show startup message
-(setq inhibit-startup-message t)
-
 ;; Also auto refresh dired, but be quiet about it
 (setq global-auto-revert-non-file-buffers t)
 (setq auto-revert-verbose nil)
@@ -52,9 +36,6 @@
 
 ;; Transparently open compressed files
 (auto-compression-mode t)
-
-;; Enable syntax highlighting for older Emacsen that have it off
-(global-font-lock-mode t)
 
 ;; Answering just 'y' or 'n' will do
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -75,70 +56,20 @@
 ;; Remove text in active region if inserting text
 (delete-selection-mode 1)
 
-;; Don't highlight matches with jump-char - it's distracting
-(setq jump-char-lazy-highlight-face nil)
-
-;; Always display line and column numbers
-(setq line-number-mode t)
-(setq column-number-mode t)
-
 ;; Lines should be 80 characters wide, not 72
 (setq fill-column 80)
-
-;; Save a list of recent files visited. (open recent file with C-x f)
-(recentf-mode 1)
-(setq recentf-max-saved-items 100) ;; just 20 is too recent
-
-;; Save minibuffer history
-(savehist-mode 1)
-(setq savehist-file "~/.emacs.d/savehist")
-(setq history-length 1000)
-
-;; Undo/redo window configuration with C-c <left>/<right>
-(winner-mode 1)
 
 ;; Never insert tabs
 (set-default 'indent-tabs-mode nil)
 
-;; Show me empty lines after buffer end
-(set-default 'indicate-empty-lines t)
-
-;; Easily navigate sillycased words
-(global-subword-mode 1)
-
 ;; Don't break lines for me, please
 (setq-default truncate-lines t)
-
-;; Allow recursive minibuffers
-(setq enable-recursive-minibuffers t)
 
 ;; Don't be so stingy on the memory, we have lots now. It's the distant future.
 (setq gc-cons-threshold 20000000)
 
-;; org-mode: Don't ruin S-arrow to switch windows please (use M-+ and M-- instead to toggle)
-(setq org-replace-disputed-keys t)
-
-;; Fontify org-mode code blocks
-(setq org-src-fontify-natively t)
-
 ;; Sentences do not need double spaces to end. Period.
 (set-default 'sentence-end-double-space nil)
-
-;; 80 chars is a good width.
-(set-default 'fill-column 80)
-
-;; Show trailing whitespace
-;; (setq-default show-trailing-whitespace 0)
-
-;;;; Hooks
-
-;; Clean trailling whitespace when saving
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; handle shell colours
-(add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
-
-(add-hook 'text-mode-hook (lambda() (visual-line-mode 1)))
 
 ;; Add support to iso key
 (require 'iso-transl)
