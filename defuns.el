59e99dcb999b5c38fc1c47b8eb9b28e344435c89
marcwebbie <marcwebbie@gmail.com>
2015-01-03 19:17:10 -0300

    Add occur functions

--- defuns.el
+++ defuns.el
@@ -316,8 +316,25 @@ Otherwise point moves to beginning of line."
     (when file
       (find-file file))))
 
+;;;; Mastering Emacs
 
-;;;; My functions
+(defun get-buffers-matching-mode (mode)
+  "Returns a list of buffers where their major-mode is equal to MODE"
+  (let ((buffer-mode-matches '()))
+   (dolist (buf (buffer-list))
+     (with-current-buffer buf
+       (if (eq mode major-mode)
+           (add-to-list 'buffer-mode-matches buf))))
+   buffer-mode-matches))
+
+(defun multi-occur-in-this-mode ()
+  "Show all lines matching REGEXP in buffers with this major mode."
+  (interactive)
+  (multi-occur
+   (get-buffers-matching-mode major-mode)
+   (car (occur-read-primary-args))))
+
+;;;; Marcwebbie
 
 (defun mw/spell-dictionary (choice)
   "Switch between language dictionaries."
