2c9228125780db08bdff06f2835ab28738ba61dc
marcwebbie <marcwebbie@gmail.com>
2015-02-18 06:48:41 -0200

    Update appearance settings

--- init.el
+++ init.el
@@ -10,7 +10,8 @@
  '(custom-safe-themes
    (quote
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "e56f1b1c1daec5dbddc50abd00fcd00f6ce4079f4a7f66052cf16d96412a09a9" "c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default)))
- '(magit-use-overlays nil))
+ '(magit-use-overlays nil)
+ '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -57,7 +58,7 @@
 ;;;; Appearance
 ;;----------------------------------------------------------------------------
 
-(load-theme 'solarized-light :no-confirm)
+(load-theme 'noctilux :no-confirm)
 
 (setq visible-bell t
       font-lock-maximum-decoration t
@@ -69,10 +70,10 @@
   (tooltip-mode -1)
   (blink-cursor-mode -1)
   (mw/set-best-font '(
-                      ("Ubuntu Mono" 16)
+                      ("Input Mono Narrow" 14)
                       ("Inconsolata" 14)
-                      ("Input Mono Narrow" 12)
-                      ("Monaco" 12)
+                      ("Ubuntu Mono" 16)
+                      ("Monaco" 14)
                       ("Monospace" 12)
                       ("DejaVu Sans Mono" 14)
                       ("Source Code Pro" 14)
