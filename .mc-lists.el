f8b099a77319c184170267aacc91cc7a1b4e5416
marcwebbie <marcwebbie@gmail.com>
2015-05-08 02:17:24 -0300

    Update mc-list

--- .mc-lists.el
+++ .mc-lists.el
@@ -12,6 +12,7 @@
         drag-stuff-up
         duplicate-current-line-or-region
         end-of-visual-line
+        eval-print-last-sexp
         hippie-expand-no-case-fold
         indent-for-tab-command
         kill-region
