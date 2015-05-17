12a9ca26c9b6ed74c7c00d1a3f3a6b1d4eaac6e0
marcwebbie <marcwebbie@gmail.com>
2015-05-17 18:15:00 -0300

    Update setup-buffers

--- lisp/setup-buffers.el
+++ lisp/setup-buffers.el
@@ -1,6 +1,17 @@
 (require 'use-package)
 (require 'bind-key)
 
+(use-package files
+  :bind (("C-c R" . rename-this-buffer-and-file)
+         ("C-c D" . delete-this-buffer-and-file))
+  :config
+  (progn
+    (setq auto-save-default nil)
+    (global-auto-revert-mode 1)
+    (setq make-backup-files nil) ; stop creating those backup~ files
+    (setq auto-save-default nil) ; stop creating those #autosave# files
+    (add-hook 'after-save-hook 'whitespace-cleanup)
+    (add-hook 'before-save-hook 'delete-trailing-whitespace)))
 
 (provide 'setup-buffers)
 
