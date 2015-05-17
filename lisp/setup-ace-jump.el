d8833bc88bcd5e551b2c70b2edd1e1e03a64f6cc
marcwebbie <marcwebbie@gmail.com>
2015-05-17 18:14:46 -0300

    Add keybinding to ace-jump

--- lisp/setup-ace-jump.el
+++ lisp/setup-ace-jump.el
@@ -3,6 +3,7 @@
 (use-package ace-jump-mode
   :defer 3
   :bind (("C-c SPC" . ace-jump-mode)
+         ("C-." . ace-jump-mode)
          ("C-c C-SPC" . ace-jump-mode-pop-mark))
   :config
   (setq ace-jump-mode-case-fold t)
