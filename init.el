e69103e7d7169a4155789a834a4a2dfb4bab6b3a
marcwebbie <marcwebbie@gmail.com>
2015-09-11 15:00:58 -0300

    Auto added packages line

--- init.el
+++ init.el
@@ -1,6 +1,13 @@
 ;;============================================================
 ;; Defaults
 ;;============================================================
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defconst *is-a-mac* (eq system-type 'darwin))
 
 (setq inhibit-startup-message t)
