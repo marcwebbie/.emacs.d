d11a0c9823e7aeef3840a0a1816f30dc50af2d14
marcwebbie <marcwebbie@gmail.com>
2015-09-12 23:40:50 -0300

    Update custom config

--- custom.el
+++ custom.el
@@ -7,12 +7,18 @@
    (quote
     (elpy-module-company elpy-module-eldoc elpy-module-flymake elpy-module-pyvenv elpy-module-sane-defaults)))
  '(elpy-test-runner (quote elpy-test-pytest-runner))
+ '(git-gutter:added-sign "█")
+ '(git-gutter:deleted-sign "█")
+ '(git-gutter:modified-sign "█")
+ '(git-gutter:window-width 1)
  '(markdown-header-face-1 ((t (:inherit org-level-1))))
  '(markdown-header-face-2 ((t (:inherit org-level-2))))
  '(markdown-header-face-3 ((t (:inherit org-level-3))))
  '(markdown-header-face-4 ((t (:inherit org-level-4))))
  '(markdown-header-face-5 ((t (:inherit org-level-5))))
- '(package-selected-packages (quote (package-build shut-up epl git commander f dash s))))
+ '(package-selected-packages
+   (quote
+    (sublimity package-build shut-up epl git commander f dash s))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
