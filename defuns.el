142055673d72f046e23a8a87570518f4569cf36f
marcwebbie <marcwebbie@gmail.com>
2015-09-13 03:12:14 -0300

    Add python debug functions

--- defuns.el
+++ defuns.el
@@ -400,6 +400,53 @@ Symbols matching the text at point are put first in the completion list."
    (get-buffers-matching-mode major-mode)
    (car (occur-read-primary-args))))
 
+
+(defun python--add-debug-highlight ()
+  "Adds a highlighter for use by `python--pdb-breakpoint-string'"
+  (highlight-lines-matching-regexp "## DEBUG ##\\s-*$" 'hi-red-b))
+
+(add-hook 'python-mode-hook 'python--add-debug-highlight)
+
+(defvar python--pdb-breakpoint-string "import pdb; pdb.set_trace() ## DEBUG ##"
+  "Python breakpoint string used by `python-insert-breakpoint'")
+
+
+(defun python-insert-breakpoint ()
+  "Inserts a python breakpoint using `pdb'"
+  (interactive)
+  (back-to-indentation)
+  ;; this preserves the correct indentation in case the line above
+  ;; point is a nested block
+  (split-line)
+  (insert python--pdb-breakpoint-string))
+;; (define-key python-mode-map (kbd "<f5>") 'python-insert-breakpoint)
+
+
+(defadvice compile (before ad-compile-smart activate)
+  "Advises `compile' so it sets the argument COMINT to t
+if breakpoints are present in `python-mode' files"
+  (when (derived-mode-p major-mode 'python-mode)
+    (save-excursion
+      (save-match-data
+        (goto-char (point-min))
+        (if (re-search-forward (concat "^\\s-*" python--pdb-breakpoint-string "$")
+                               (point-max) t)
+            ;; set COMINT argument to `t'.
+            (ad-set-arg 1 t))))))
+
+(defun etc-log-tail-handler ()
+  "Clean auto-revert-tail mode"
+  (end-of-buffer)
+  (make-variable-buffer-local 'auto-revert-interval)
+  (setq auto-revert-interval 1)
+  (auto-revert-set-timer)
+  (make-variable-buffer-local 'auto-revert-verbose)
+  (setq auto-revert-verbose nil)
+  (read-only-mode t)
+  (font-lock-mode 0)
+  (when (fboundp 'show-smartparens-mode)
+    (show-smartparens-mode 0)))
+
 ;;;; Marcwebbie
 
 (defun mw/spell-dictionary (choice)
