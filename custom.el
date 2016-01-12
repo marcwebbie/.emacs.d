3f84e5ec4dffc7bac4e76c70821a2c0db76bc567
marcwebbie <marcwebbie@gmail.com>
2016-01-12 03:32:27 -0200

    Update custom set variables

--- custom.el
+++ custom.el
@@ -11,9 +11,9 @@
    (quote
     (elpy-module-company elpy-module-eldoc elpy-module-sane-defaults)))
  '(elpy-test-runner (quote elpy-test-pytest-runner))
- '(git-gutter:added-sign "█")
- '(git-gutter:deleted-sign "█")
- '(git-gutter:modified-sign "█")
+ ;; '(git-gutter:added-sign "●")
+ ;; '(git-gutter:deleted-sign "●")
+ ;; '(git-gutter:modified-sign "●")
  '(git-gutter:window-width 1)
  '(markdown-header-face-1 ((t (:inherit org-level-1))))
  '(markdown-header-face-2 ((t (:inherit org-level-2))))
@@ -29,4 +29,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:foreground "red" :weight bold :height 1.8)))))
