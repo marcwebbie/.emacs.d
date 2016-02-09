0fce40506144fc2fed803c2e9cbc7682efc81c53
marcwebbie <marcwebbie@gmail.com>
2016-02-09 19:12:29 -0200

    Update copy-filename-to-clipboard keybinding

--- init.el
+++ init.el
@@ -235,7 +235,7 @@
   (bind-key "C-a" 'back-to-indentation-or-beginning-of-line)
   (bind-key "C-z" 'zap-up-to-char)
   (bind-key "C-|" 'align-regexp)
-  (bind-key "C-c c" 'copy-file-name-to-clipboard)
+  (bind-key "C-x c" 'copy-file-name-to-clipboard)
 
   (bind-key "M-j" (λ (join-line -1)))
   (bind-key "M-h" 'kill-to-beginning-of-line)
