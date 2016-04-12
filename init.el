4d4302b52181d86e817751c2aea4b83d1613244d
marcwebbie <marcwebbie@gmail.com>
2016-04-12 17:34:00 -0300

    Add irony-mode

--- init.el
+++ init.el
@@ -921,5 +921,37 @@
          ("Gemfile$" . ruby-mode)
          ("Guardfile$" . ruby-mode)
          ("Fastfile$" . ruby-mode)
+         ("Gymfile$" . ruby-mode)
+         ("Matchfile$" . ruby-mode)
+         ("Appfile$" . ruby-mode)))
+
+(use-package irony
+  :ensure t
+  :defer t
+  :mode (("\\.h$" . objc-mode)
+         ("\\.mm$" . objc-mode)
+         ("\\.m$" . objc-mode)
          )
-  )
+  :init
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'objc-mode-hook 'irony-mode)
+  :config
+  (setq c-indent-level 4)
+  (setq c-basic-offset 4)
+  ;; replace the `completion-at-point' and `complete-symbol' bindings in
+  ;; irony-mode's buffers by irony-mode's function
+  (defun my-irony-mode-hook ()
+    (define-key irony-mode-map [remap completion-at-point]
+      'irony-completion-at-point-async)
+    (define-key irony-mode-map [remap complete-symbol]
+      'irony-completion-at-point-async))
+  (add-hook 'irony-mode-hook 'my-irony-mode-hook)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+  (use-package company-irony
+    :ensure t
+    :config
+    (eval-after-load 'company
+      '(add-to-list 'company-backends 'company-irony))))
+
