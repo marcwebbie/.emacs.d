091a8652e7ba1ab6c8ba50cff7dcde2ee25c2adc
marcwebbie <marcwebbie@gmail.com>
2016-05-11 15:49:25 -0300

    Change pad modeline size

--- custom.el
+++ custom.el
@@ -7,6 +7,7 @@
  '(custom-safe-themes
    (quote
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(darkokai-mode-line-padding 1)
  '(elpy-modules
    (quote
     (elpy-module-company elpy-module-eldoc elpy-module-sane-defaults)))
