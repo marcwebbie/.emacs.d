88cc825ce833bc1fe66b5f858c2e6bd5dba49bd3
marcwebbie <marcwebbie@gmail.com>
2016-07-03 13:26:14 -0300

    Add kill-line to mc-list

--- .mc-lists.el
+++ .mc-lists.el
@@ -28,6 +28,7 @@
         org-delete-char
         org-end-of-line
         org-force-self-insert
+        org-kill-line
         org-return-indent
         org-self-insert-command
         org-yank
