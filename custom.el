605549d35b3bb0efa68963af4e19703e8f9fe4ae
Marcwebbie <marcwebbie@gmail.com>
2022-07-19 17:16:24 +0200

    Update custom

--- custom.el
+++ custom.el
@@ -5,13 +5,11 @@
  ;; If there is more than one, they won't work right.
  '(bmkp-last-as-first-bookmark-file "/Users/marc/.emacs.d/bookmarks")
  '(custom-safe-themes
-   (quote
-    ("f9aede508e587fe21bcfc0a85e1ec7d27312d9587e686a6f5afdbb0d220eab50" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+   '("f9aede508e587fe21bcfc0a85e1ec7d27312d9587e686a6f5afdbb0d220eab50" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default))
  '(darkokai-mode-line-padding 1)
  '(elpy-modules
-   (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-sane-defaults)))
- '(elpy-test-runner (quote elpy-test-pytest-runner))
+   '(elpy-module-company elpy-module-eldoc elpy-module-sane-defaults))
+ '(elpy-test-runner 'elpy-test-pytest-runner)
  '(git-gutter:window-width 1)
  '(markdown-header-face-1 ((t (:inherit org-level-1))))
  '(markdown-header-face-2 ((t (:inherit org-level-2))))
@@ -19,8 +17,7 @@
  '(markdown-header-face-4 ((t (:inherit org-level-4))))
  '(markdown-header-face-5 ((t (:inherit org-level-5))))
  '(package-selected-packages
-   (quote
-    (python-pytest jedi rjsx-mode jenkinsfile-mode robots-txt-mode docker vagrant flatui-theme prassee-theme ujelly-theme material-theme google-translate nov-mode nov anything-tramp monokai-theme magit-auto-revert-mode darkokai-theme fullframe puppet-mode visual-regexp-steroids php-mode company-irony irony less-css-mode emmet-mode pig-mode browse-at-remote gist markdown-mode nose py-test auto-virtualenv evil spaceline zzz-to-char which-key avy ivy-hydra counsel highlight-symbol gitconfig-mode diff-hl super-save paradox cyphejor flycheck pythonic yasnippet highlight-indentation find-file-in-project company company-anaconda anaconda-mode pyenv-mode pyenv solarized-dark-theme zenburn-theme coffee-mode yaml-mode elpy pyvenv pip-requirements eldoc-mode json-mode js2-mode jade-mode jinja2-mode web-mode files smart-mode-line imenu-anywhere region-bindings-mode smartparens expand-region multiple-cursors drag-stuff hippie guide-key restclient rainbow-delimiters golden-ratio smex projectile visual-regexp flx-ido ido-ubiquitous ido-vertical-mode tdd-mode git-timemachine git-gutter magit ace-window ace-jump-mode dired-x exec-path-from-shell company-tern color-moccur bookmark+ nyan-mode idomenu helm bm realgud sublimity package-build shut-up epl git commander f dash s)))
+   '(python-pytest jedi rjsx-mode jenkinsfile-mode robots-txt-mode docker vagrant flatui-theme prassee-theme ujelly-theme material-theme google-translate nov-mode nov anything-tramp monokai-theme magit-auto-revert-mode darkokai-theme fullframe puppet-mode visual-regexp-steroids php-mode company-irony irony less-css-mode emmet-mode pig-mode browse-at-remote gist markdown-mode nose py-test auto-virtualenv evil spaceline zzz-to-char which-key avy ivy-hydra counsel highlight-symbol gitconfig-mode diff-hl super-save paradox cyphejor flycheck pythonic yasnippet highlight-indentation find-file-in-project company company-anaconda anaconda-mode pyenv-mode pyenv solarized-dark-theme zenburn-theme coffee-mode yaml-mode elpy pyvenv pip-requirements eldoc-mode json-mode js2-mode jade-mode jinja2-mode web-mode files smart-mode-line imenu-anywhere region-bindings-mode smartparens expand-region multiple-cursors drag-stuff hippie guide-key restclient rainbow-delimiters golden-ratio smex projectile visual-regexp flx-ido ido-ubiquitous ido-vertical-mode tdd-mode git-timemachine git-gutter magit ace-window ace-jump-mode dired-x exec-path-from-shell company-tern color-moccur bookmark+ nyan-mode idomenu helm bm realgud sublimity package-build shut-up epl git commander f dash s))
  '(paradox-github-token t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
