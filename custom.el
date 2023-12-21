7bfa54ea5530f2dc63baaf17f8c3500a0edef530
Marcwebbie <marcwebbie@gmail.com>
2023-12-21 21:48:49 +0100

    Update config lsp and modeline

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(auto-save-visited-interval 2)
  '(auto-save-visited-mode t)
  '(custom-safe-themes
-   '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "eab123a5ed21463c780e17fc44f9ffc3e501655b966729a2d5a2072832abd3ac" default))
+   '("7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "eab123a5ed21463c780e17fc44f9ffc3e501655b966729a2d5a2072832abd3ac" default))
  '(package-selected-packages
    '(quelpa python-pytest restclient dimmer symbol-overlay mmm-mode web-mode py-isort blacken lsp-pyright flycheck hippie-expand lsp-ui lsp-imenu lsp-mode flymake-ruff elpy prism projectile auto-virtualenv beacon diff-hl jenkinsfile-mode groovy-mode deadgrep real-auto-save jinja2-mode chatgpt-shell all-the-icons-dired nerd-icons-dired nerd-icons color-theme-sanityinc-tomorrow spacemacs-theme spaceline spaceline-config all-the-icons move-text highlight-indentation highlight-indentation-mode sublimity dashboard treesit-auto aggressive-indent aggressive-indent-mode zoom selected region-bindings-mode smartparens expand-region multiple-cursors drag-stuff wgrep orderless kind-icon cape corfu-terminal corfu marginalia vertico embark-consult embark consult avy json-mode yaml-mode magit which-key))
  '(zoom-mode t nil (zoom))
